3a9228afe04a6a858f57e840f430ba418e2ec6bc
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-27 01:10:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,17 +9,8 @@ end
 new_array
 end
 
-def reduce(array)
-end
-
-
 def reduce(array, starting_point=nil)
-  if starting_point
-    single_value = starting_point
-    counter = 0
-  else
-    single_value = array[0]
-    counter = 1
+  starting_point ? single_value = starting_point, counter = 0 : single_value = array[0], counter = 1
   end
   while counter < array.length
     single_value = yield(single_value, array[counter])
