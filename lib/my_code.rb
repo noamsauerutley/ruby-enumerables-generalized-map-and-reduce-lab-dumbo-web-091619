5178efc213c633258fb4a979b5107c3795879ec4
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-27 01:08:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# Your Code Here
+
+def map(array)
+new_array = []
+counter = 0
+while array[counter]
+  new_array.push(yield(array[counter]))
+  counter +=1
+end
+new_array
+end
+
+def reduce(array)
+end
+
+
+def reduce(array, starting_point=nil)
+  if starting_point
+    single_value = starting_point
+    counter = 0
+  else
+    single_value = array[0]
+    counter = 1
+  end
+  while counter < array.length
+    single_value = yield(single_value, array[counter])
+    counter+=1
+  end
+  single_value
+end
